f7118b2e0735fead60a95fa9203acd7a62a5ee0f
Your Name <you@example.com>
2022-01-18 04:24:08 -0300

    more refactor

--- bot.py
+++ bot.py
@@ -20,19 +20,21 @@ GAS_PRICE = 5000000000
 
 # SECONDS LEFT BET AT
 SECONDS_LEFT = 8
+# SLEEP SECS AFTER BET
+SLEEP = 60
 
 # IF TRUE, WHEN BNB BALANCE IS BELLOW BNB_LIMIT
 # CLAIM ROUNDS INSIDE RANGE (CURRENT ROUND - RANGE)
 RANGE = 100
 BNB_LIMIT = 0.1
-CLAIM = True
+CLAIM = False
 
 # V2 CONTRACT
-predictionContract = w3.eth.contract(address=PREDICTION_CONTRACT, abi=PREDICTION_ABI)
+PREDICTION_CONTRACT = w3.eth.contract(address=PREDICTION_CONTRACT, abi=PREDICTION_ABI)
 
 
 def bet_bull(value, epoch):
-    txn = predictionContract.functions.betBull(epoch).buildTransaction({
+    txn = PREDICTION_CONTRACT.functions.betBull(epoch).buildTransaction({
         'from': ADDRESS,
         'nonce': w3.eth.getTransactionCount(ADDRESS),
         'value': value,
@@ -45,7 +47,7 @@ def bet_bull(value, epoch):
 
 
 def bet_bear(value, epoch):
-    txn = predictionContract.functions.betBear(epoch).buildTransaction({
+    txn = PREDICTION_CONTRACT.functions.betBear(epoch).buildTransaction({
         'from': ADDRESS,
         'nonce': w3.eth.getTransactionCount(ADDRESS),
         'value': value,
@@ -58,39 +60,35 @@ def bet_bear(value, epoch):
     
     
 def claim(epochs):
-    txn = predictionContract.functions.claim(epochs).buildTransaction({
+    txn = PREDICTION_CONTRACT.functions.claim(epochs).buildTransaction({
         'from': ADDRESS,
-        'nonce': w3.eth.getTransactionCount(ADDRESS),
-        'value': 0,
-        'gas': 800000,
-        'gasPrice': 5000000000,
+        'nonce': w3.eth.getTransactionCount(ADDRESS)
     })
     signed_txn = w3.eth.account.signTransaction(txn, private_key=PRIVATE_KEY)
     w3.eth.sendRawTransaction(signed_txn.rawTransaction)
     print(f'{w3.eth.waitForTransactionReceipt(signed_txn.hash)}')
 
     
-def fetchClaimable():
-    epochs = []
-    current = predictionContract.functions.currentEpoch().call()
+def fetch_claimable():
+    current = PREDICTION_CONTRACT.functions.currentEpoch().call()
     epoch = current - 2
     stop = epoch - RANGE
-
+    epochs = []
     while epoch >= stop:
-        claimable = predictionContract.functions.claimable(epoch, ADDRESS).call()
+        claimable = PREDICTION_CONTRACT.functions.claimable(epoch, ADDRESS).call()
         if claimable:
             epochs.append(epoch)
         epoch -= 1
     return epochs
 
 
-def handleClaim():
+def handle_claim():
     myBalance = w3.eth.getBalance(ADDRESS)
     myBalance = w3.fromWei(myBalance, 'ether')
     print(f'My Balance:  {myBalance:.5f} | Limit {BNB_LIMIT}')
     if myBalance <= BNB_LIMIT:
         print(f'Balance Bellow {BNB_LIMIT}, fetching claimable rounds...%')
-        epochs = fetchClaimable()
+        epochs = fetch_claimable()
         if len(epochs) > 0:
             print(f'Attempting to claim {len(epochs)} rounds...%\n {epochs}')
             claim(epochs)
@@ -98,7 +96,7 @@ def handleClaim():
             print(f'Sorry, no rounds to claim')
     
 
-def makeBet(epoch):
+def make_bet(epoch):
     """
     Add your bet logic here
 
@@ -114,31 +112,31 @@ def makeBet(epoch):
         bet_bear(value, epoch)
         
 
-def newRound():
+def new_round():
     try:
-        current_epoch = predictionContract.functions.currentEpoch().call()
-        data = predictionContract.functions.rounds(current_epoch).call()
+        current_epoch = PREDICTION_CONTRACT.functions.currentEpoch().call()
+        data = PREDICTION_CONTRACT.functions.rounds(current_epoch).call()
         bet_time = dt.datetime.fromtimestamp(data[2]) - dt.timedelta(seconds=SECONDS_LEFT)
-        if CLAIM:
-            handleClaim()
         print(f'New round: #{current_epoch}')
         return [bet_time, current_epoch]
     except Exception as error:
-        print(f'New round fail - {error}')
+        print(f'{error}')
 
 
 def run():
-    current_round = newRound()
+    current_round = new_round()
     while True:
         try:
             now = dt.datetime.now()
             if now >= current_round[0]:
-                makeBet(current_round[1])
-                time.sleep(40)
-                current_round = newRound()
+                make_bet(current_round[1])
+                time.sleep(SLEEP)
+                if CLAIM:
+                    handle_claim()
+                current_round = new_round()
         except Exception as error:
             print(f'Restarting...% {error}')
-            current_round = newRound()
+            current_round = new_round()
 
 
 if __name__ == '__main__':
